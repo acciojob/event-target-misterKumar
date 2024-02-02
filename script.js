d42091860f0c62828ecd40caa4a3e3ebf59d0d25
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-02 07:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    this.listeners.get(event).add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    if (this.listeners.has(event)) {
+      const eventListeners = this.listeners.get(event);
+      eventListeners.delete(callback);
+      if (eventListeners.size === 0) {
+        this.listeners.delete(event);
+      }
+    }
+  }
+
+  dispatchEvent(event) {
+    if (this.listeners.has(event)) {
+      const eventListeners = this.listeners.get(event);
+      for (const listener of eventListeners) {
+        listener();
+      }
+    }
+  }
+}
